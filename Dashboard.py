ec5312853f4a359a6e0d17353332f7981cd1eed5
Nayma04 <126800876+Nayma04@users.noreply.github.com>
2025-03-14 20:49:19 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+import matplotlib.pyplot as plt
+
+#Dashboard
 st.write(
     """
     # Dashboard Projek Akhir
